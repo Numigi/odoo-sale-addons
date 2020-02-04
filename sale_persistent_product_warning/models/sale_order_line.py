8dff43259372c12966fcc9efedf8bc94546baab4
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-02-04 12:57:40 -0500

    Fix case where product warning has been disabled (#31)

--- sale_persistent_product_warning/models/sale_order_line.py
+++ sale_persistent_product_warning/models/sale_order_line.py
@@ -10,5 +10,12 @@ class SaleOrderLine(models.Model):
 
     product_warning = fields.Text(
         "Product Warning Message",
-        related='product_id.sale_line_warn_msg',
+        compute='_compute_product_warning',
     )
+
+    def _compute_product_warning(self):
+        lines_with_warnings = self.filtered(
+            lambda l: l.product_id.sale_line_warn != 'no-message'
+        )
+        for line in lines_with_warnings:
+            line.product_warning = line.product_id.sale_line_warn_msg
