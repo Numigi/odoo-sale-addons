56775e3eaea9c7bb8c2b695471005188f046058d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-02-04 11:46:33 -0500

    Fix case where product warning has been disabled

---
+++ sale_persistent_product_warning/tests/test_sale_order_line.py
@@ -0,0 +1,44 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from ddt import ddt, data
+from odoo.tests.common import SavepointCase
+
+
+@ddt
+class TestSaleOrderLine(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.message = 'Some warning message'
+
+        cls.product = cls.env['product.product'].create({
+            'name': 'My Product',
+            'sale_line_warn': 'warning',
+            'sale_line_warn_msg': cls.message,
+        })
+
+        cls.sale_order = cls.env['sale.order'].create({
+            'partner_id': cls.env.user.partner_id.id,
+            'order_line': [
+                (0, 0, {
+                    'product_id': cls.product.id,
+                    'name': cls.product.name,
+                    'product_uom': cls.env.ref('uom.product_uom_unit').id,
+                    'product_uom_qty': 1,
+                })
+            ]
+        })
+
+        cls.line = cls.sale_order.order_line
+
+    def test_if_no_message__so_line_message_is_empty(self):
+        self.product.sale_line_warn = 'no-message'
+        assert not self.line.product_warning
+
+    @data('warning', 'block')
+    def test_if_message_enabled__so_line_message_is_displayed(self, warning_config):
+        self.product.sale_line_warn = warning_config
+        assert self.line.product_warning == self.message
