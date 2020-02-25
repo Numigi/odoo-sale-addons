d0ad3350038e9b89b10580ddb4a10fcd50b20485
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-02-25 13:32:35 -0500

    Update stock_move.py

--- sale_warranty/models/stock_move.py
+++ sale_warranty/models/stock_move.py
@@ -1,8 +1,6 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 from odoo import models
 
 
