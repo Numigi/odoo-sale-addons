60e0573cb474c4060c959d21915f20169e77262d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-03-23 15:52:14 -0400

    Codacy

--- sale_rental/tests/test_stock_warehouse.py
+++ sale_rental/tests/test_stock_warehouse.py
@@ -1,9 +1,7 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import pytest
 from odoo.tests import common
-from odoo.exceptions import ValidationError
 
 
 class TestWarehouse(common.SavepointCase):
