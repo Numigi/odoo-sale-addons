4aa240b7788745c47873ce3ebde62c56194f9f8e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-01 14:42:53 -0400

    Update stock_picking_add_transit_rental/tests/test_stock_picking.py

--- stock_picking_add_transit_rental/tests/test_stock_picking.py
+++ stock_picking_add_transit_rental/tests/test_stock_picking.py
@@ -1,7 +1,6 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import pytest
 from odoo.exceptions import ValidationError
 from odoo.addons.stock_picking_add_transit.tests.common import (
     StockPickingAddTransitCase,
