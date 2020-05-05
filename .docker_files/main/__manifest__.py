dcec0232daf5ee60084dcd2fd7fc5989f445d3b7
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-05 17:39:55 -0400

    Remove stock_rental_conversion

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -26,11 +26,6 @@
         "sale_warranty_extension",
         "sale_warranty_lead_on_expiry",
         "sale_whole_order_invoiced",
-<<<<<<< HEAD
-        "stock_picking_add_transit_rental",
-=======
->>>>>>> ee98208... Add module
-        "stock_rental_conversion",
     ],
     "installable": True,
 }
