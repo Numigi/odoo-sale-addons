bfd042e6987e445a4b64cff976376150ae137b89
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-09-16 17:05:55 -0400

    TA#18414 Add sale_privilege_level modules

---
+++ sale_privilege_level/models/sale_privilege_level.py
@@ -0,0 +1,17 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SalePrivilegeLevel(models.Model):
+
+    _name = "sale.privilege.level"
+    _description = "Sale Privilege Level"
+
+    sequence = fields.Integer()
+    name = fields.Char(required=True, translate=True)
+    description = fields.Char(translate=True)
+    active = fields.Boolean(default=True)
+
+    partner_ids = fields.One2many("res.partner", "privilege_level_id")
