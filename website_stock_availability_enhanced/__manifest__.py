f78102de5214b29fe1dacbd4b75509d1804ade47
🌴Cuong, Nguyen Minh Tran Manh <cuong.nmtm@komit-consulting.com>
2020-10-19 04:17:14 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- website_stock_availability_enhanced/__manifest__.py
+++ website_stock_availability_enhanced/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Stock Availability Enhanced",
-    "summary": "",
+    "summary": "Indicate if the inventory is below a threshold without blocking the sale",
     "version": "12.0.1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
