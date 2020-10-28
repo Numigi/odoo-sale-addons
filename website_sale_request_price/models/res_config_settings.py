859502cabf958f27ca79adae8f5468dcf7ab7d73
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-10-28 17:43:53 +0700

    Update field's strings, translation

--- website_sale_request_price/models/res_config_settings.py
+++ website_sale_request_price/models/res_config_settings.py
@@ -8,21 +8,17 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     website_sale_request_price = fields.Boolean(
-        string="Hide prices from a threshold and allow to request for quotation",
         config_parameter="website_sale_request_price"
     )
     website_sale_request_price_threshold = fields.Float(
-        string="Threshold",
         config_parameter="website_sale_request_price_threshold"
     )
     website_sale_request_price_mail_template = fields.Many2one(
         "mail.template",
-        string="Mail Template",
         config_parameter="website_sale_request_price_mail_template"
     )
     website_sale_request_price_sales_team = fields.Many2one(
         "crm.team",
-        string="Sales Team",
         config_parameter="website_sale_request_price_sales_team"
     )
 
