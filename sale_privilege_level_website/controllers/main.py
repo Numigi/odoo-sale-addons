16744593ea31510da474cdbe76ecb3a2d8b2a2ee
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-04 11:54:43 -0500

    Fix context propagation

--- sale_privilege_level_website/controllers/main.py
+++ sale_privilege_level_website/controllers/main.py
@@ -10,8 +10,5 @@ class WebsiteSaleWithPrivilegeLevels(WebsiteSaleDelivery):
     def _get_shop_payment_values(self, order, **kwargs):
         new_context = {"sale_privilege_level_partner_id": order.partner_id.id}
         order = order.with_context(**new_context)
-
-        new_context = dict(request.env.context, **new_context)
-        request.env.context = frozendict(new_context)
-
+        request.env.context = frozendict(request.env.context, **new_context)
         return super()._get_shop_payment_values(order, **kwargs)
