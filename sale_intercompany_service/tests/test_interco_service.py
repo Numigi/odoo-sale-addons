e668e406f130f8d6e445b090da094a3565a4c636
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-17 14:00:44 -0400

    Implement interco service wizard

--- sale_intercompany_service/tests/test_interco_service.py
+++ sale_intercompany_service/tests/test_interco_service.py
@@ -4,33 +4,33 @@
 from odoo.tests.common import SavepointCase
 
 
-class TestIntercoService(SavepointCase):
+class IntercoServiceCase(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.discount = 20
+        cls.interco_discount = 20
 
-        cls.mother_company = cls.env["res.company"].create(
-            {"name": "Mother Company", "interco_service_discount": cls.discount}
-        )
-        cls.subsidiary = cls.env["res.company"].create({"name": "Subsidiary"})
+        cls.mother_company = cls._create_company("Mother Company")
+        cls.mother_company.interco_service_discount = cls.interco_discount
+
+        cls.subsidiary = cls._create_company("Subsidiary")
         cls.subsidiary_partner = cls.subsidiary.partner_id
 
         cls.interco_position = cls.env["account.fiscal.position"].create(
-            {"name": "Interco Fiscal Position"}
+            {"name": "Interco Fiscal Position", "company_id": cls.mother_company.id}
         )
-        cls.subsidiary_partner.fiscal_position_id = cls.interco_position
+        cls._set_fiscal_position(
+            cls.subsidiary_partner, cls.mother_company, cls.interco_position
+        )
 
         cls.customer_position = cls.env["account.fiscal.position"].create(
             {"name": "Customer's Fiscal Position"}
         )
         cls.customer = cls.env["res.partner"].create(
-            {
-                "name": "My Customer",
-                "company_id": None,
-                "fiscal_position_id": cls.customer_position.id,
-            }
+            {"name": "My Customer", "company_id": None}
         )
+        cls._set_fiscal_position(cls.customer, cls.subsidiary, cls.customer_position)
+
         cls.delivery_address = cls.env["res.partner"].create(
             {"name": "Delivery Address", "type": "delivery", "company_id": None}
         )
@@ -46,11 +46,12 @@ class TestIntercoService(SavepointCase):
 
         cls.order = cls.env["sale.order"].create(
             {
+                "company_id": cls.mother_company.id,
                 "partner_id": cls.customer.id,
                 "partner_invoice_id": cls.subsidiary.partner_id.id,
                 "partner_shipping_id": cls.delivery_address.id,
                 "is_interco_service": True,
-                "line_ids": [
+                "order_line": [
                     (
                         0,
                         0,
@@ -63,28 +64,80 @@ class TestIntercoService(SavepointCase):
                 ],
             }
         )
+        cls.order_line = cls.order.order_line
+        cls.order_line.discount = 10
+
+        cls.user = cls.env.ref("base.user_demo")
+        cls.user.groups_id = cls.env.ref("sales_team.group_sale_salesman_all_leads")
+        cls.user.write(
+            {
+                "company_id": cls.mother_company.id,
+                "company_ids": [(6, 0, [cls.mother_company.id])],
+            }
+        )
+        cls.env = cls.env(user=cls.user)
 
         cls.wizard = cls.env["sale.interco.service.invoice"].create(
             {"order_id": cls.order.id}
         )
 
+    @staticmethod
+    def _set_fiscal_position(partner, company, position):
+        partner.with_context(
+            force_company=company.id
+        ).property_account_position_id = position
+
+    @classmethod
+    def _create_company(cls, name):
+        account_chart = cls.env.ref("l10n_generic_coa.configurable_chart_template")
+        company = cls.env["res.company"].create({"name": name})
+        cls.env.user.company_ids |= company
+        cls.env.user.company_id = company
+        account_chart.try_loading_for_current_company()
+        return company
+
+
+class TestWizard(IntercoServiceCase):
     def test_wizard_fields(self):
         assert self.wizard.company_id == self.mother_company
 
         assert self.wizard.interco_company_id == self.subsidiary
         assert self.wizard.interco_partner_id == self.subsidiary_partner
         assert self.wizard.interco_position_id == self.interco_position
-        assert self.wizard.discount == self.discount
+        assert self.wizard.discount == self.interco_discount
 
         assert self.wizard.customer_id == self.customer
-        assert self.wizard.customer_position == self.customer_position.display_name
+        assert self.wizard.customer_position_id == self.customer_position
+        assert self.wizard.customer_position_name == self.customer_position.display_name
         assert self.wizard.customer_delivery_address_id == self.delivery_address
 
     def test_fiscal_position_from_delivery_address(self):
-        self.customer.fiscal_position_id = None
-        self.delivery_address.fiscal_position_id = self.customer_position
-        assert self.wizard.customer_position == self.customer_position.display_name
+        self._set_fiscal_position(self.customer, self.subsidiary, None)
+        self._set_fiscal_position(
+            self.delivery_address, self.subsidiary, self.customer_position
+        )
+        assert self.wizard.customer_position_id == self.customer_position
 
     def test_no_fiscal_position_defined(self):
-        self.customer.fiscal_position_id = None
-        assert not self.wizard.customer_position
+        self._set_fiscal_position(self.customer, self.subsidiary, None)
+        assert not self.wizard.customer_position_id
+
+
+class TestIntercoInvoice(IntercoServiceCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.order.sudo(cls.user).action_confirm()
+        cls.wizard.validate()
+
+        cls.invoice_line = cls.order_line.invoice_lines
+        cls.invoice = cls.invoice_line.invoice_id
+
+    def test_invoice_fields(self):
+        assert self.invoice.partner_id == self.subsidiary_partner
+        assert self.invoice.partner_shipping_id == self.subsidiary_partner
+        assert self.invoice.fiscal_position_id == self.interco_position
+
+    def test_invoice_line_discount(self):
+        # The interco discount is added to the customer discount
+        assert self.invoice_line.discount == 28  # 100 * (1 - (1 - 10%) * (1 - 20%))
--- sale_intercompany_service/wizard/sale_interco_service_invoice.py
+++ sale_intercompany_service/wizard/sale_interco_service_invoice.py
@@ -1,7 +1,7 @@
 # © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleIntercoServiceInvoice(models.TransientModel):
@@ -11,13 +11,91 @@ class SaleIntercoServiceInvoice(models.TransientModel):
 
     order_id = fields.Many2one("sale.order")
 
-    company_id = fields.Many2one("res.company")
-    discount = fields.Float(related="company_id.interco_service_discount")
+    company_id = fields.Many2one("res.company", related="order_id.company_id")
+    discount = fields.Float(
+        related="company_id.interco_service_discount", related_sudo=True
+    )
 
-    interco_company_id = fields.Many2one("res.company")
-    interco_partner_id = fields.Many2one("res.partner")
-    interco_position_id = fields.Many2one("account.fiscal.position")
+    interco_company_id = fields.Many2one(
+        "res.company", compute="_compute_interco_company_id"
+    )
+    interco_partner_id = fields.Many2one(
+        "res.partner", compute="_compute_interco_partner_id"
+    )
+    interco_position_id = fields.Many2one(
+        "account.fiscal.position",
+        compute="_compute_interco_position_id",
+        compute_sudo=True,
+    )
 
-    customer_id = fields.Many2one("res.partner")
-    customer_position = fields.Char()
-    customer_delivery_address_id = fields.Many2one("res.partner")
+    customer_id = fields.Many2one("res.partner", related="order_id.partner_id")
+    customer_position_id = fields.Many2one(
+        "account.fiscal.position",
+        compute="_compute_customer_position_id",
+        compute_sudo=True,
+    )
+    customer_position_name = fields.Char(
+        related="customer_position_id.display_name", related_sudo=True
+    )
+    customer_delivery_address_id = fields.Many2one(
+        "res.partner", related="order_id.partner_shipping_id"
+    )
+
+    @api.depends("interco_partner_id")
+    def _compute_interco_company_id(self):
+        for wizard in self:
+            wizard.interco_company_id = wizard.sudo()._get_interco_company()
+
+    def _get_interco_company(self):
+        partner = self.interco_partner_id.commercial_partner_id
+        return self.env["res.company"].search(
+            [("partner_id", "=", partner.id)], limit=1
+        )
+
+    @api.depends("interco_partner_id")
+    def _compute_interco_position_id(self):
+        for wizard in self:
+            wizard.interco_position_id = wizard.sudo()._get_interco_position()
+
+    def _get_interco_position(self):
+        return (
+            self.env["account.fiscal.position"]
+            .with_context(force_company=self.company_id.id)
+            .get_fiscal_position(self.interco_partner_id.id)
+        )
+
+    def _compute_customer_position_id(self):
+        for wizard in self:
+            wizard.customer_position_id = wizard.sudo()._get_customer_position()
+
+    def _get_customer_position(self):
+        return (
+            self.env["account.fiscal.position"]
+            .with_context(force_company=self.interco_company_id.id)
+            .get_fiscal_position(
+                self.customer_id.id, self.customer_delivery_address_id.id
+            )
+        )
+
+    def _compute_interco_partner_id(self):
+        for wizard in self:
+            wizard.interco_partner_id = wizard.sudo().order_id.partner_invoice_id
+
+    def validate(self):
+        invoice_ids = self.order_id.action_invoice_create()
+        invoices = self.env["account.invoice"].browse(invoice_ids)
+
+        for invoice in invoices:
+            self._update_invoice(invoice)
+
+        return invoices[:1].get_formview_action()
+
+    def _update_invoice(self, invoice):
+        invoice.partner_shipping_id = self.interco_partner_id.id
+        self._update_invoice_line_discount(invoice)
+
+    def _update_invoice_line_discount(self, invoice):
+        for line in invoice.invoice_line_ids:
+            line.discount = 100 * (
+                1 - (1 - line.discount / 100) * (1 - self.discount / 100)
+            )
