d7bffd29cff3346bcb3bb5c75d4af62e2ff94dbe
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-06-30 14:22:53 -0400

    Allow multiple targets in payroll preparation wizard

--- commission_payroll_preparation/wizard/commission_payroll_period_selection.py
+++ commission_payroll_preparation/wizard/commission_payroll_period_selection.py
@@ -1,29 +1,39 @@
 # © 2021 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class CommissionPayrollPeriodSelection(models.TransientModel):
     _name = "commission.payroll.period.selection"
     _description = "Commission Payroll Period Selection"
 
-    target_id = fields.Many2one("commission.target")
+    target_ids = fields.Many2many(
+        "commission.target",
+        "commission_payroll_period_selection_target_rel",
+        "wizard_id",
+        "target_id",
+    )
     period = fields.Many2one("payroll.period")
 
-    def confirm(self):
-        amount = self.target_id.commissions_total
-        self._create_payroll_entry(
-            
-        )
+    @api.model
+    def default_get(self, fields_list):
+        defaults = super().default_get(fields_list)
+        defaults["target_ids"] = [(6, 0, self._context.get("active_ids"))]
+        return defaults
 
-    def _create_payroll_entry(self):
+    def confirm(self):
+        for target in self.target_ids:
+            amount = target.commissions_total
+            self._create_payroll_entry(target)
+
+    def _create_payroll_entry(self, target):
         return self.env["payroll.preparation.line"].create(
             {
-                "company_id": self.target_id.company_id.id,
+                "company_id": target.company_id.id,
                 "period_id": self.period.id,
-                "employee_id": self.target_id.employee_id.id,
-                "target_id": self.target_id.id,
-                "amount": self.target_id.commissions_total,
+                "employee_id": target.employee_id.id,
+                "target_id": target.id,
+                "amount": target.commissions_total,
             }
         )
