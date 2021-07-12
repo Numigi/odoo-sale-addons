1e0de649c2d093e4e580f497444487b0e21f2025
Gabriel Gosselin Roberge <56849645+gabgosrob@users.noreply.github.com>
2021-07-12 11:34:28 -0400

    simplify test fixture

--- commission_prorata/tests/test_wizard.py
+++ commission_prorata/tests/test_wizard.py
@@ -1,40 +1,11 @@
 # © 2021 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import pytest
 from odoo.exceptions import ValidationError
-from .common import TestPayrollCase
+from .common import TestProrataCase
 
 
-class TestWizard(TestPayrollCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        cls.target_amount = 1000
-        cls.fixed_rate = 0.05
-        cls.target = cls.env["commission.target"].create(
-            {
-                "employee_id": cls.employee.id,
-                "category_id": cls.category.id,
-                "target_amount": cls.target_amount,
-                "date_range_id": cls.date_range.id,
-                "rate_type": "fixed",
-                "fixed_rate": cls.fixed_rate,
-                "state": "confirmed"
-            }
-        )
-
-        cls.wizard = cls.env["commission.payroll.preparation.wizard"].create(
-            {
-                "target_ids": [(6, 0, [cls.target.id])],
-                "period": cls.period.id,
-            }
-        )
-
-        cls.invoiced_amount = 500
-        cls._create_invoice(amount=cls.invoiced_amount)
-        cls.target.compute()
+class TestWizard(TestProrataCase):
 
     def test_create_payroll_default_prorata(self):
         self.wizard.confirm()
