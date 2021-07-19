7d1035155da2956d6778d0d86ec7292a46a7e081
Gabriel <56849645+gabgosrob@users.noreply.github.com>
2021-07-19 09:19:04 -0400

    TA#38003 Prorata fix (#130)
    * update eligible amount on target compute
    
    * update default wizard prorata
    
    * revert default wizard prorata
    
    * add last computed datetime
    
    * update commission translation + add commission_prorata translation
    
    * fix field update permission error
    
    * Update fr.po
    
    Co-authored-by: Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>

--- commission_prorata/models/commission_target.py
+++ commission_prorata/models/commission_target.py
@@ -8,9 +8,15 @@ class CommissionTarget(models.Model):
     _inherit = "commission.target"
 
     prorata_days_worked = fields.Float(default=1, readonly=True)
-    eligible_amount = fields.Monetary(
-        readonly=True, compute="_compute_eligible_amount", store=True
-    )
+    eligible_amount = fields.Monetary(default=0, readonly=True)
+
+    def compute(self):
+        super().compute()
+        for target in self:
+            target.sudo()._update_eligible_amount()
+
+    def _update_eligible_amount(self):
+        self.eligible_amount = self.total_amount * self.prorata_days_worked
 
     @api.depends("eligible_amount", "already_generated")
     def _compute_left_to_generate(self):
