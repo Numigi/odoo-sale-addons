607d2e4851c95f307fcff90bdd1f7f8a6cf30beb
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2021-07-23 19:26:52 +0700

    T#007957 - PO18000199 - #37031 - Taxes de la compagnie par défaut sur les SO

---
+++ sale_order_default_taxes/README.rst
@@ -0,0 +1,12 @@
+Sale Order Default Taxes
+========================
+This module adds feature to suggest configured Sale Tax when the user creates Sale Order Lines
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+* Komit (https://komit-consulting.com)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ sale_order_default_taxes/tests/test_sale_order_default_taxes.py
@@ -0,0 +1,45 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import Form
+from odoo.tests.common import SavepointCase, post_install
+
+
+@post_install(True)
+class TestCRMAssignByArea(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company = cls.env.ref("base.main_company")
+        cls.partner = cls.env.ref("base.main_partner")
+        cls.tax_20 = cls.env["account.tax"].create({"name": "Tax 20%", "amount": 20})
+        cls.tax_30 = cls.env["account.tax"].create({"name": "Tax 30%", "amount": 30})
+        cls.product = cls.env["product.product"].create({"name": "Product"})
+
+    def test_sale_order_no_default_taxes(self):
+        self.company.account_sale_tax_id = False
+        with Form(self.env["sale.order"]) as form:
+            form.partner_id = self.partner
+            with form.order_line.new() as line:
+                line.product_id = self.product
+            so = form.save()
+            self.assertTrue(not so.order_line.tax_id)
+
+    def test_sale_order_default_taxes_from_company(self):
+        self.company.account_sale_tax_id = self.tax_20
+        with Form(self.env["sale.order"]) as form:
+            form.partner_id = self.partner
+            with form.order_line.new() as line:
+                line.product_id = self.product
+            so = form.save()
+            self.assertEqual(so.order_line.tax_id, self.tax_20)
+
+    def test_sale_order_default_taxes_from_product(self):
+        self.company.account_sale_tax_id = self.tax_20
+        self.product.write({"taxes_id": [(6, 0, [self.tax_30.id])]})
+        with Form(self.env["sale.order"]) as form:
+            form.partner_id = self.partner
+            with form.order_line.new() as line:
+                line.product_id = self.product
+            so = form.save()
+            self.assertEqual(so.order_line.tax_id, self.tax_30)
