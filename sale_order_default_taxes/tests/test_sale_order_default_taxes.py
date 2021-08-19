61f23e7b5f3718fef78b3bcdad58d01a0c934091
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-08-19 15:15:04 -0400

    Refactor and fix unit tests

--- sale_order_default_taxes/tests/test_sale_order_default_taxes.py
+++ sale_order_default_taxes/tests/test_sale_order_default_taxes.py
@@ -1,6 +1,7 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from contextlib import contextmanager
 from odoo.tests import Form
 from odoo.tests.common import SavepointCase
 
@@ -15,30 +16,29 @@ class TestCRMAssignByArea(SavepointCase):
         cls.tax_30 = cls.env["account.tax"].create({"name": "Tax 30%", "amount": 30})
         cls.product = cls.env["product.product"].create({"name": "Product"})
 
-    def test_sale_order_no_default_taxes(self):
+    def test_no_default_taxes(self):
         self.company.account_sale_tax_id = False
-        with Form(self.env["sale.order"]) as form:
-            form.partner_id = self.partner
-            with form.order_line.new() as line:
-                line.product_id = self.product
-            so = form.save()
-            self.assertTrue(not so.order_line.tax_id)
+        self.product.taxes_id = False
+        with self._prepare_sale_order() as so:
+            assert not so.order_line.tax_id
 
-    def test_sale_order_default_taxes_from_company(self):
+    def test_taxes_from_company(self):
         self.company.account_sale_tax_id = self.tax_20
-        with Form(self.env["sale.order"]) as form:
-            form.partner_id = self.partner
-            with form.order_line.new() as line:
-                line.product_id = self.product
-            so = form.save()
-            self.assertEqual(so.order_line.tax_id, self.tax_20)
+        self.product.taxes_id = False
+        with self._prepare_sale_order() as so:
+            assert so.order_line.tax_id == self.tax_20
 
-    def test_sale_order_default_taxes_from_product(self):
+    def test_taxes_from_product(self):
         self.company.account_sale_tax_id = self.tax_20
-        self.product.write({"taxes_id": [(6, 0, [self.tax_30.id])]})
+        self.product.taxes_id = self.tax_30
+        with self._prepare_sale_order() as so:
+            assert so.order_line.tax_id == self.tax_30
+
+    @contextmanager
+    def _prepare_sale_order(self):
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.partner
             with form.order_line.new() as line:
                 line.product_id = self.product
             so = form.save()
-            self.assertEqual(so.order_line.tax_id, self.tax_30)
+            yield so
