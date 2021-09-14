e55fa6c89cffd8471a3cbbf4a58d5ac42c2c88a8
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-09-14 14:16:43 -0400

    TA#37362 add module website_event_message_unpublished

---
+++ website_event_message_unpublished/models/mail_message.py
@@ -0,0 +1,18 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class MailMessage(models.Model):
+
+    _inherit = "mail.message"
+
+    @api.model
+    def create(self, vals):
+        message = super().create(vals)
+
+        if message.model == "event.event":
+            message.website_published = False
+
+        return message
