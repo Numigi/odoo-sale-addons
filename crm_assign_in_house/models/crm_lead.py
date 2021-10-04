c62f8b5164ed078cbeaa3f252dae5477b754a7ab
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-10-04 15:20:23 -0400

    TA#35005 add in_house and user_id as commercial fields (#154)

--- crm_assign_in_house/models/crm_lead.py
+++ crm_assign_in_house/models/crm_lead.py
@@ -5,11 +5,12 @@ from odoo import api, fields, models
 
 
 class CrmLead(models.Model):
+
     _inherit = "crm.lead"
 
     in_house = fields.Boolean(related="partner_id.in_house")
 
     @api.onchange("partner_id")
-    def _onchange_partner_id(self):
+    def _onchange_partner_in_house(self):
         if self.partner_id and self.partner_id.in_house:
             self.user_id = self.partner_id.user_id
--- crm_assign_in_house/models/res_partner.py
+++ crm_assign_in_house/models/res_partner.py
@@ -1,10 +1,17 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     in_house = fields.Boolean("In-house")
+
+    @api.model
+    def _commercial_fields(self):
+        res = super()._commercial_fields()
+        res.append("in_house")
+        res.append("user_id")
+        return res
