516ec1cc20549b27792ba411b527a01342956512
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-06 09:34:28 -0500

    TA#41003 add module contract_invoice_offset (#159)
    * TA#41003 add module contract_invoice_offset
    
    * Add business logic for the module
    
    * Remove monthly option
    
    The monthly option was based on a relative delta.
    This relative delta used the start/end date of the next period.
    
    However, when computing the next period, the invoicing offset is used.
    Thus, there was a recursive dependency.
    
    * Add documentation

---
+++ contract_invoice_offset/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Contract Invoice Offset",
+    "version": "1.0.0",
+    "category": "Contract Management",
+    "author": "Numigi",
+    "website": "https://numigi.com",
+    "depends": [
+        "contract",
+        "contract_invoice_start_end_dates",
+    ],
+    "data": [
+        "views/contract_line.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ contract_invoice_offset/models/contract_line.py
@@ -0,0 +1,63 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class ContractLine(models.Model):
+
+    _inherit = "contract.line"
+
+    invoicing_offset_interval = fields.Integer()
+    invoicing_offset_rule_type = fields.Selection(
+        [
+            ("daily", "Day(s)"),
+            ("weekly", "Week(s)"),
+        ],
+        default="daily",
+        string="Invoicing Offset Type",
+    )
+
+    @api.depends(
+        "invoicing_offset_interval",
+        "invoicing_offset_rule_type",
+    )
+    def _compute_recurring_invoicing_offset(self):
+        for rec in self:
+            rec.recurring_invoicing_offset = rec._get_invoicing_offset()
+
+    def _get_invoicing_offset(self):
+        rule_type = self.invoicing_offset_rule_type
+
+        if rule_type == "daily":
+            return self._get_daily_invoicing_offset()
+
+        elif rule_type == "weekly":
+            return self._get_weekly_invoicing_offset()
+
+    def _get_daily_invoicing_offset(self):
+        return -self.invoicing_offset_interval
+
+    def _get_weekly_invoicing_offset(self):
+        return -self.invoicing_offset_interval * 7
+
+    def _check_recurring_next_date_start_date(self):
+        pass
+
+    def _check_last_date_invoiced(self):
+        pass
+
+    @api.onchange("invoicing_offset_interval", "invoicing_offset_rule_type")
+    def _onchange_invoicing_offset(self):
+        for line in self.filtered('date_start'):
+            line.__set_recurring_next_date()
+
+    def __set_recurring_next_date(self):
+        self.recurring_next_date = self.get_next_invoice_date(
+            self.next_period_date_start,
+            self.recurring_invoicing_type,
+            self.recurring_invoicing_offset,
+            self.recurring_rule_type,
+            self.recurring_interval,
+            max_date_end=self.date_end,
+        )
