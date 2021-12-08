44d2cb337717c59178f5937abffe58ae61a4f0dc
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-08 15:30:27 -0500

    TA#41455 fix invoiced quantity

--- sale_timesheet_invoicing_period/__manifest__.py
+++ sale_timesheet_invoicing_period/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Timesheet Invoicing Period",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
