d075a4e2499151cbda385acc5751f29cea745bf5
Aimé Jules <aimejules1@gmail.com>
2022-01-30 23:30:17 +0300

    Code review with black

--- sale_order_available_qty_popover_rental/__manifest__.py
+++ sale_order_available_qty_popover_rental/__manifest__.py
@@ -2,15 +2,15 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': "Sale Order Available Qty Popover Rental",
-    'summary': "Inherit Compute of Qty Popover",
-    'author': "Numigi",
-    'maintener': "Numigi",
-    'website': "https://bit.ly/numigi-com",
+    "name": "Sale Order Available Qty Popover Rental",
+    "summary": "Inherit Compute of Qty Popover",
+    "author": "Numigi",
+    "maintener": "Numigi",
+    "website": "https://bit.ly/numigi-com",
     "license": "LGPL-3",
-    'category': 'Sales',
-    'version': '0.1',
-    'depends': ['stock_rental', 'sale_rental', 'sale_order_available_qty_popover'],
-    'data': [],
+    "category": "Sales",
+    "version": "0.1",
+    "depends": ["stock_rental", "sale_rental", "sale_order_available_qty_popover"],
+    "data": [],
     "installable": True,
 }
--- sale_order_available_qty_popover_rental/models/sale_order_line.py
+++ sale_order_available_qty_popover_rental/models/sale_order_line.py
@@ -5,7 +5,7 @@ from odoo import models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _get_available_qty_for_popover(self):
         if self.order_id.is_rental is False:
@@ -14,21 +14,31 @@ class SaleOrderLine(models.Model):
             return self._get_available_qty_for_popover_rental()
 
     def _get_available_qty_for_popover_sale(self):
-        stock_quant_domain = [('product_id', '=', self.product_id.id),
-                              ('location_id', 'in', self._get_location_for_sale())]
-        stock_quants_ids = self.env['stock.quant'].search(stock_quant_domain)
-        return sum(stock_quants_ids.mapped('quantity'))
+        stock_quant_domain = [
+            ("product_id", "=", self.product_id.id),
+            ("location_id", "in", self._get_location_for_sale()),
+        ]
+        stock_quants_ids = self.env["stock.quant"].search(stock_quant_domain)
+        return sum(stock_quants_ids.mapped("quantity"))
 
     def _get_available_qty_for_popover_rental(self):
-        stock_quant_domain = [('product_id', '=', self.product_id.id),
-                              ('location_id', 'in', self._get_location_for_rental())]
-        stock_quants_ids = self.env['stock.quant'].search(stock_quant_domain)
-        return sum(stock_quants_ids.mapped('quantity'))
+        stock_quant_domain = [
+            ("product_id", "=", self.product_id.id),
+            ("location_id", "in", self._get_location_for_rental()),
+        ]
+        stock_quants_ids = self.env["stock.quant"].search(stock_quant_domain)
+        return sum(stock_quants_ids.mapped("quantity"))
 
     def _get_location_for_sale(self):
-        location_domain = [('usage', '=', 'internal'), ('is_rental_customer_location', '=', False)]
-        return self.env['stock.location'].search(location_domain).ids
+        location_domain = [
+            ("usage", "=", "internal"),
+            ("is_rental_customer_location", "=", False),
+        ]
+        return self.env["stock.location"].search(location_domain).ids
 
     def _get_location_for_rental(self):
-        location_domain = [('usage', '=', 'internal'), ('is_rental_customer_location', '=', True)]
-        return self.env['stock.location'].search(location_domain).ids
+        location_domain = [
+            ("usage", "=", "internal"),
+            ("is_rental_customer_location", "=", True),
+        ]
+        return self.env["stock.location"].search(location_domain).ids
