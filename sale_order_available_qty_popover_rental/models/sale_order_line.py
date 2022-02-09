0e0ff42c9cd61306121daf0e3b6fc1ebc3cd3d2b
Roumani <rahma.roumani@smile-maroc.com>
2022-02-09 10:06:31 +0100

    [IMPL] : Adding "sale_order_available_qty_popover_rental" module - FIX : Removing unnecessary dependencies from "sale_order_line_readonly_conditions" and "sale_kit" modules - FIX: Fixing "sale_order_line_readonly_conditions" module installation

---
+++ sale_order_available_qty_popover_rental/models/sale_order_line.py
@@ -0,0 +1,24 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = "sale.order.line"
+
+    def _get_available_qty_for_popover(self):
+        self.ensure_one()
+        if self.product_id:
+            res = self.product_id.with_context(
+                from_sale_order=True, is_rental_sale=self.order_id.is_rental
+            )._compute_quantities_dict(
+                self._context.get("lot_id"),
+                self._context.get("owner_id"),
+                self._context.get("package_id"),
+                self._context.get("from_date"),
+                self._context.get("to_date"),
+            )
+            return res.get(self.product_id.id).get("qty_available")
+        return self.product_id.with_context(company_owned=True).qty_available
