96cb3091e8d620eb7744d4d83fa573b83a7b5d48
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2022-02-25 00:35:47 +0700

    T#009225 - PO18000217 - #40775 - Ajustement du calcul du taux de complétion pour les Locations

---
+++ sale_delivery_completion_rental/README.rst
@@ -0,0 +1,24 @@
+Sale Delivery Completion Rental
+===============================
+
+This module adds a smart button on the sale order.
+
+.. image:: static/description/smart_button.png
+
+This button shows the rate of return completion of the order.
+
+This rate represents the percentage of stockable (and consummable) products
+that were returned from the customer.
+
+When clicking on the button, the list of unreturned stock pickings for this
+order is displayed.
+
+.. image:: static/description/stock_picking_list.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ sale_delivery_completion_rental/models/sale_order.py
@@ -0,0 +1,23 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    return_rate = fields.Char(compute="_compute_return_rate", string="Return")
+
+    def _compute_return_rate(self):
+        for order in self:
+            order.return_rate = order._get_return_rate()
+
+    def _get_return_rate(self):
+        lines_without_services = self.order_line.filtered(
+            lambda l: l.product_id.type in ("product", "consu")
+        )
+        returned = sum(lines_without_services.mapped(lambda l: l.rental_returned_qty))
+        total_qty = sum(lines_without_services.mapped(lambda l: l.product_uom_qty))
+        completion = (returned / total_qty) if total_qty else 1
+        return "{}%".format(int(completion * 100))
