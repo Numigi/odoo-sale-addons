d4234c741a7c5df6b82ef4fb9f269f34115e15b8
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2022-03-21 11:10:47 -0400

    TA#40362 add module sale_rental_pricelist (#189)
    * TA#40362 add module sale_rental_pricelist
    
    * TA#40362 add module sale_privilege_level_rental_pricelist
    
    * Prevent conflicts on module tests
    
    * Add missing module to dockerfile

--- sale_privilege_level_rental_pricelist/models/res_partner.py
+++ sale_privilege_level_rental_pricelist/models/res_partner.py
@@ -1,32 +1,30 @@
-# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import threading
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
 
     _inherit = "res.partner"
 
-    property_product_pricelist = fields.Many2one(inverse=None)
-
-    def _compute_product_pricelist(self):
+    @api.depends("privilege_level_id", "country_id")
+    def _compute_rental_pricelist(self):
         if _is_testing_other_module(self._context):
-            return super()._compute_product_pricelist()
+            return super()._compute_rental_pricelist()
 
         for partner in self:
-            available_pricelists = partner.sudo()._get_available_pricelists()
-            partner.property_product_pricelist = available_pricelists[:1]
+            partner.rental_pricelist_id = partner.sudo()._get_rental_pricelist()
 
-    def _get_available_pricelists(self):
+    def _get_rental_pricelist(self):
         privilege_level = self.get_privilege_level()
         pricelist_entries = (
-            privilege_level.mapped("pricelist_ids")
+            privilege_level.mapped("rental_pricelist_ids")
             .sorted("sequence")
             .filtered(lambda e: e.matches_partner(self))
         )
-        return pricelist_entries.mapped("pricelist_id")
+        return pricelist_entries.mapped("pricelist_id")[:1]
 
 
 def _is_testing_other_module(context):
