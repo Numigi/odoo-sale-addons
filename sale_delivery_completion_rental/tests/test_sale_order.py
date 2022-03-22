fbdb4e84a69860d0d1dfe32c2843f82655798f9f
Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>
2022-03-22 17:39:04 -0400

    TA#40773 add Sale Rental Status (#194)
    * T#009225 - PO18000217 - #40775 - Ajustement du calcul du taux de complétion pour les Locations
    
    * Update readme
    
    * Store sale.order completion_rate because it is displayed on tree view
    
    * Store return_rate and add to tree view
    
    * Update translation
    
    * Add missing compute dependencies
    
    * Fix automatic tests
    
    * T#009226 - PO18000218 - #40773 - Suivi des locations par statuts de locations
    
    * Fix automatic test
    
    Co-authored-by: cuong.nmtm <cuong.nmtm@komit-consulting.com>

---
+++ sale_delivery_completion_rental/tests/test_sale_order.py
@@ -0,0 +1,90 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import SavepointCase
+
+
+class TestSaleOrder(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.customer = cls.env["res.partner"].create(
+            {"name": "Customer A", "customer": True}
+        )
+        cls.product_1 = cls.env["product.product"].create({"name": "Product 1"})
+        cls.product_2 = cls.env["product.product"].create({"name": "Product 2"})
+        cls.so = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.customer.id,
+                "is_rental": True,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product_1.id,
+                            "product_uom_qty": 100,
+                            "price_unit": 100,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product_2.id,
+                            "product_uom_qty": 100,
+                            "price_unit": 100,
+                        },
+                    ),
+                ],
+            }
+        )
+        cls.so.action_confirm()
+
+    def test_not_yet_returned(self):
+        assert self.so.return_rate == "0%"
+
+    def test_partial_returned(self):
+        rental_picking = self.so.picking_ids.filtered(
+            lambda r: r.state not in ("done", "cancel")
+            and any(r.move_lines.mapped("location_id.is_rental_customer_location"))
+        )
+        
+        rental_picking.move_ids_without_package.write({"quantity_done": 10})
+        rental_picking.button_validate()
+        rental_picking.move_ids_without_package._action_done()
+        assert self.so.return_rate == "10%"
+
+    def test_fully_returned(self):
+        rental_picking = self.so.picking_ids.filtered(
+            lambda r: r.state not in ("done", "cancel")
+            and any(r.move_lines.mapped("location_id.is_rental_customer_location"))
+        )
+        
+        rental_picking.move_ids_without_package.write({"quantity_done": 100})
+        rental_picking.button_validate()
+        assert self.so.return_rate == "100%"
+
+    def test_if_no_stockable_product_then_fully_returned(self):
+        product_3 = self.env["product.product"].create(
+            {"name": "Product 3", "type": "service"}
+        )
+        so = self.env["sale.order"].create(
+            {
+                "partner_id": self.customer.id,
+                "is_rental": True,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": product_3.id,
+                            "product_uom_qty": 100,
+                            "price_unit": 100,
+                        },
+                    )
+                ],
+            }
+        )
+        so.action_confirm()
+        assert so.return_rate == "100%"
