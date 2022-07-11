8798c9a3efa6f0c1d92379110ce92a989e9ab374
Julie LeBrun <38360781+TumbaoJu@users.noreply.github.com>
2022-07-11 10:29:30 -0400

    Correction de la version

--- sale_order_line_margin_amount/__manifest__.py
+++ sale_order_line_margin_amount/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Line Margin Amount",
-    "version": "2.0.0",
+    "version": "1.1.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
