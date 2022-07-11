a549d840b124456620ec6866e7452974fddaa3f8
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-07-11 16:24:56 +0100

    TA#44212 add module sale_order_line_form_view (#226)
    * TA#44212 add module sale_order_line_form_view
    
    * Update read me
    
    Co-authored-by: Julie LeBrun <38360781+TumbaoJu@users.noreply.github.com>
    Co-authored-by: Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>

---
+++ sale_order_line_form_view/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Line Form View  ",
+    "summary": "Sale order line in form view",
+    "version": "1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
+}
