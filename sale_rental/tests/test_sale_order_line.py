bf39ac1a564c055b00b0b2707199026b6017ea3f
Houda BEN TALEB <houda.bentaleb@gmail.com>
2023-01-10 18:24:38 +0100

    TA#47979 [MIG]:correction test_rental_service_readonly_fields

--- sale_rental/tests/test_sale_order_line.py
+++ sale_rental/tests/test_sale_order_line.py
@@ -78,7 +78,7 @@ class TestKitRental(KitRentalCase):
     def test_rental_service_readonly_fields(self):
         self.add_kit_on_sale_order()
         service = self.get_rental_service_lines()
-        service.invalidate_cache()
+        #service.invalidate_cache()
         service.flush()
         assert service.kit_reference_readonly
         assert service.product_readonly
