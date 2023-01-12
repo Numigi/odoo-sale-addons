804ae0b1f98691e82fd37a4a8d25bf2b07fc8592
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2023-01-12 15:31:20 +0100

    TA#48810 [IMP]  sale_order_available_qty_popover_alternative : virtual_qty (#243)

--- sale_order_available_qty_popover_alternative/__manifest__.py
+++ sale_order_available_qty_popover_alternative/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Available Qty Popover Alternative",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
