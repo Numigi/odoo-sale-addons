4036fa358e553102618e5f083ee4ed183c64cd02
houdaBENTALEB <houda.bentaleb@gmail.com>
2023-03-13 12:44:05 +0100

    TA#50766 [FIX]sale_commitment_date_update_mrp: unit tests

--- sale_commitment_date_update_mrp/wizard/sale_commitment_date_update_mrp.py
+++ sale_commitment_date_update_mrp/wizard/sale_commitment_date_update_mrp.py
@@ -1,8 +1,12 @@
 # © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+import logging
 from datetime import timedelta
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleCommitmentDateUpdateMrp(models.TransientModel):
