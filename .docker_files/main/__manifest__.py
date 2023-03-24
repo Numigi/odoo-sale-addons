287d5c63211d7cc82efd4fca598e050781bdf729
houdaBENTALEB <houda.bentaleb@gmail.com>
2023-03-24 23:11:48 +0000

    TA#50766 [DEL]: comment sale_order_url_tracking in manifest of main module

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -37,7 +37,7 @@
         #"sale_order_line_margin_amount",
         "sale_order_line_readonly_conditions",
         "sale_order_margin_percent",
-        "sale_order_url_tracking",
+        #"sale_order_url_tracking",
         "sale_order_weight",
         "sale_persistent_product_warning",
         "sale_privilege_level",
