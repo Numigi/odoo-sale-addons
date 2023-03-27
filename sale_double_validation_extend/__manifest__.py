dc662ebb4ceef0f567cade80710676beb226696a
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-03-27 12:36:57 -0400

    TA#51844 [ADD] Module sale_double_validation_extend (#255)
    * TA#51844 [ADD] Module Double validation for Sales
    
    * TA#51844 [ADD] Module sale_double_validation_extend

---
+++ sale_double_validation_extend/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Double Validation Extend",
+    "summary": "Extend the module Sale Double Validation to blocks user confirming in the creation of Sale Order",
+    "version": "1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["sale_double_validation"],
+    "data": ["views/sale_order.xml"],
+    "installable": True,
+}
