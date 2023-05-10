9fda9c1fae6c38b45019ff18f97b66c6021db0dd
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-05-10 19:18:12 +0100

    TA#53238[ADD] sale_approve

--- sale_approve/__manifest__.py
+++ sale_approve/__manifest__.py
@@ -1,8 +1,8 @@
 # © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Sale Double Validation Speciic",
-    "summary": "Switch steps of the module Sale Double Validation",
+    "name": "Sale Approve",
+    "summary": "Switch steps of the module Sale approve",
     "version": "1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
