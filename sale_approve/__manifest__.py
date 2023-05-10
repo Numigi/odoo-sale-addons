45bb8f9dc5ea3ab166a5c4c105f72e98f6f3eaec
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-05-10 19:17:02 +0100

    TA#53238[ADD] sale_approve

---
+++ sale_approve/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Double Validation Speciic",
+    "summary": "Switch steps of the module Sale Double Validation",
+    "version": "1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order.xml",
+             "views/company.xml"],
+    "installable": True,
+}
---
+++ sale_approve/models/company.py
@@ -0,0 +1,24 @@
+# © 2023 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    so_double_validation = fields.Selection(
+        [
+            ("one_step", "Confirm sale orders in one step"),
+            ("two_step", "Get 2 levels of approvals to confirm a sale order"),
+        ],
+        string="Levels of Sale Order Approvals",
+        default="one_step",
+        help="Provide a double validation mechanism for sales",
+    )
+
+    so_double_validation_amount = fields.Monetary(
+        string="Sale Order double validation amount",
+        default=5000,
+        help="Minimum amount for which a double validation is required",
+    )
