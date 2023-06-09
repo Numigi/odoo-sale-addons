8b5edde2a2d3a284d1e89ed6f512e76955e37351
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-06-09 16:53:07 +0100

    TA#53238 [i18n] Fr translation

--- sale_approve/__manifest__.py
+++ sale_approve/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Approve",
-    "summary": "Switch steps of the module Sale approve",
+    "summary": "Add a setp request for approval in sale workflow",
     "version": "1.0.1",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
