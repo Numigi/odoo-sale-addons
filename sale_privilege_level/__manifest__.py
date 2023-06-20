a62aad8b6f19dc73b60bbdfc61aaee001bdf4abd
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-06-20 17:04:37 +0300

    TA#53494 [UPD] sale_privilege_level : adding features (#277)

--- sale_privilege_level/__manifest__.py
+++ sale_privilege_level/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Privilege Level",
-    "version": "1.1.0",
+    "version": "1.2.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
