ee9b8b67cd76fa9143240592d6e08e33ac8717fd
majouda <majdaelmariouli@gmail.com>
2023-07-03 16:45:10 -0400

    TA#52053 [ADD] migrating module sale_project_milestone

--- sale_project_milestone/models/product_template.py
+++ sale_project_milestone/models/product_template.py
@@ -1,8 +1,7 @@
-# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2023 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models, api
 
 
 class ProductTemplate(models.Model):
