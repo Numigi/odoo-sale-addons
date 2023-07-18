7a42b2dd26fa4fbf2084c042a003d1ad9c2ce754
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-07-18 17:03:39 +0300

    TA#54895 [ADD] sale_privilege_level : new feature

--- sale_privilege_level/__manifest__.py
+++ sale_privilege_level/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Privilege Level",
-    "version": "1.2.0",
+    "version": "1.2.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
