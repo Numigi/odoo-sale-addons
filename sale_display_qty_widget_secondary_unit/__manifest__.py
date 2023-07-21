52e5fbba583036ff9ab4dc93a14da341be27bff0
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-07-21 06:15:59 +0300

    TA#53644 [IMP] sale_display_qty_widget_secondary_unit : render improved

--- sale_display_qty_widget_secondary_unit/__manifest__.py
+++ sale_display_qty_widget_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Display Quantity Widget Secondary Unit",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
