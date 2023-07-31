475972a2ff3392b602841b82e17c4272067774df
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-08-01 00:25:17 +0300

    TA#55502 [ADD] crm_quick_create_date_deadline : new module (#296)
    * TA#55502 [ADD] crm_quick_create_date_deadline : new module
    
    * [REM] remove unused content
    
    * Update README.rst
    
    * Update __manifest__.py
    
    ---------
    
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

---
+++ crm_quick_create_date_deadline/README.rst
@@ -0,0 +1,9 @@
+CRM Quick Create Date Deadline
+==============================
+This module adds `Date Deadline` field to the CRM Kanban View Quick Create.
+
+.. image:: static/description/expected_closing_required_kanban.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ crm_quick_create_date_deadline/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Quick Create Date Deadline",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "CRM",
+    "depends": ["crm"],
+    "summary": """
+        Add Date Deadline field to the CRM Kanban View Quick Create.
+        """,
+    "data": [
+        "views/crm_lead.xml",
+    ],
+    "installable": True,
+}
