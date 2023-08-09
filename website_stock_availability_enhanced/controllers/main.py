89aae10bef88466678a1e39f5d2bceacdf6a7bdf
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-08-09 16:11:33 -0400

    TA#55633 [FIX] Js not compatible (#300)

--- website_stock_availability_enhanced/controllers/main.py
+++ website_stock_availability_enhanced/controllers/main.py
@@ -1,8 +1,8 @@
 # © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.website_sale_stock.controllers.main import WebsiteSale
+from odoo.addons.website_sale_stock.controllers.main import WebsiteSaleStock
 
 
-class WebsiteSale(WebsiteSale):
+class WebsiteSaleStockEnhanced(WebsiteSaleStock):
     pass
