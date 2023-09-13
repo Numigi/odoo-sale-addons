a05aefd5d6ece8247cae53a4e14ffeab49ddb03f
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-09-13 12:31:07 -0400

    TA#56516 [ADD] sale_stock_availablity_popup (#309)
    * TA#56516 [ADD] sale_stock_available_popup_specific
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <a.benzbiria@gmail.com>

---
+++ sale_stock_availability_popup/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Sale Stock Availability Popup',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Hr',
+    'summary': 'Modify the available quantity popover widget in sale order lines',
+    'depends': [
+        'sale_stock',
+    ],
+    'data': [
+    ],
+    "qweb": [
+        "static/src/xml/sale_stock.xml",
+    ],
+    'installable': True,
+}
---
+++ sale_stock_availability_popup/models/sale_order.py
@@ -0,0 +1,34 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    qty_popup_color = fields.Char(compute='_compute_qty_popup_color')
+
+    @api.depends('qty_available_today', 'free_qty_today', 'product_uom_qty', 'state', 'virtual_available_at_date')
+    def _compute_qty_popup_color(self):
+        for rec in self:
+            if rec.state in ('draft', 'sent'):
+                if rec.virtual_available_at_date >= rec.product_uom_qty:
+                    rec.qty_popup_color = "text-primary"
+
+                elif 0 < rec.virtual_available_at_date < rec.product_uom_qty:
+                    rec.qty_popup_color = "text-warning"
+                else:
+                    rec.qty_popup_color = "text-danger"
+
+            elif rec.state == 'sale':
+                qty = rec.free_qty_today - rec.qty_available_today
+                if qty <= 0:
+                    rec.qty_popup_color = "text-danger"
+                else:
+                    rec.qty_popup_color = "text-primary"
+            else:
+                # The else statement is not used in the widget
+                # Used to prevent bugs related to a miss computing of all states ( depends on odoo version)
+                rec.qty_popup_color = "text-danger"
+
