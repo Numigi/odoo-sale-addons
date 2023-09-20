3a97d6b05489313b912820ff70198e215d3ff7e0
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2023-09-20 16:24:05 +0100

    Ta#56516 (#310)
    * Ta#56516 : [IMP] adjut computing

--- sale_stock_availability_popup/models/sale_order.py
+++ sale_stock_availability_popup/models/sale_order.py
@@ -8,10 +8,15 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     qty_popup_color = fields.Char(compute='_compute_qty_popup_color')
+    free_qty = fields.Float(related='product_id.free_qty')
+    qty_available = fields.Float(related='product_id.qty_available')
+    virtual_available = fields.Float(related='product_id.virtual_available')
+    outgoing_qty = fields.Float(related='product_id.outgoing_qty')
 
-    @api.depends('qty_available_today', 'free_qty_today', 'product_uom_qty', 'state', 'virtual_available_at_date')
+    @api.depends('qty_available_today', 'free_qty_today', 'product_uom_qty', 'order_id.state', 'virtual_available_at_date')
     def _compute_qty_popup_color(self):
         for rec in self:
+
             if rec.state in ('draft', 'sent'):
                 if rec.virtual_available_at_date >= rec.product_uom_qty:
                     rec.qty_popup_color = "text-primary"
@@ -22,7 +27,8 @@ class SaleOrderLine(models.Model):
                     rec.qty_popup_color = "text-danger"
 
             elif rec.state == 'sale':
-                qty = rec.free_qty_today - rec.qty_available_today
+                qty = rec.free_qty
+
                 if qty <= 0:
                     rec.qty_popup_color = "text-danger"
                 else:
