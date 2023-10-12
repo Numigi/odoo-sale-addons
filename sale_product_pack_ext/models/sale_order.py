e1666c828d93136e1734fd3a1e97ac4ec2292a34
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-10-11 22:31:44 -0400

    TA#57935 [ADD] fixe duplicate order with packs (#317)
    * TA#57935 [ADD] fixe duplicate order with packs

---
+++ sale_product_pack_ext/models/sale_order.py
@@ -0,0 +1,12 @@
+# © 2023 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.sale_product_pack.models.sale_order import SaleOrder
+
+
+def copy(self, default=None):
+    sale_copy = super(SaleOrder, self.with_context(from_copy=True)).copy(default)
+    return sale_copy
+
+SaleOrder.copy = copy
