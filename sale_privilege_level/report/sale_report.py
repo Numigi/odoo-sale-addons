ba349ad0aa61e3b06fa1dd1e486d9cc6361daeb6
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-11-20 14:42:32 -0500

    TA#55431 [UPD] Add privilege level field to sales report (#332)
    * TA#55431 [UPD] Add privilege level field to sales report

---
+++ sale_privilege_level/report/sale_report.py
@@ -0,0 +1,24 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    privilege_level_id = fields.Many2one(
+        "sale.privilege.level",
+        "Privilege Level",
+        readonly=True
+    )
+
+    def _group_by_sale(self, groupby=''):
+        res = super()._group_by_sale(groupby)
+        res += """,partner.privilege_level_id"""
+        return res
+
+    def _select_additional_fields(self, fields):
+        fields['privilege_level_id'] =\
+            ", partner.privilege_level_id as privilege_level_id"
+        return super()._select_additional_fields(fields)
