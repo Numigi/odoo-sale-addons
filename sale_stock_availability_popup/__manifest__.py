56910c00090414fe888c48cdf7f15c54d1f3573d
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-11-27 05:06:11 +0300

    TA#59393 [REF] sale_stock_availability_popup : refactoring

--- sale_stock_availability_popup/__manifest__.py
+++ sale_stock_availability_popup/__manifest__.py
@@ -2,23 +2,23 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Sale Stock Availability Popup',
-    'version': '1.1.3',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://bit.ly/numigi-com',
-    'license': 'LGPL-3',
-    'category': 'Sale',
-    'summary': 'Modify the available quantity popover widget in sale order lines',
-    'depends': [
-        'sale_stock',
+    "name": "Sale Stock Availability Popup",
+    "version": "1.1.4",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Sale",
+    "summary": "Modify the available quantity popover widget in sale order lines",
+    "depends": [
+        "sale_stock",
     ],
-    'data': [
+    "data": [
         "views/sale_order.xml",
         "views/assets.xml",
     ],
     "qweb": [
         "static/src/xml/sale_stock.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
