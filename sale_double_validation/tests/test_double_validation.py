eb41276d80acac7d4dbac702bd01236931c777aa
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-01-09 07:12:14 +0300

    TA#56848 [FIX] sale_product_pack_modifiable, sale_double_validation : rounding issue when computing, unit tests (#347)
    * TA#56848 [FIX] sale_product_pack_modifiable : rounding issue when computing
    
    * TA#56848 [CHG] sale_product_pack_modifiable : increase version
    
    * [FIX][IMP] sale_double_validation : fixing and improving unit test

--- sale_double_validation/tests/test_double_validation.py
+++ sale_double_validation/tests/test_double_validation.py
@@ -3,99 +3,117 @@
 
 from odoo.tests.common import SavepointCase
 from collections import OrderedDict
+from odoo.tools import float_compare
 
 
 class TestSaleDoubleValidation(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        context_no_mail = {'no_reset_password': True,
-                           'mail_create_nosubscribe': True, 'mail_create_nolog': True}
+        context_no_mail = {
+            "no_reset_password": True,
+            "mail_create_nosubscribe": True,
+            "mail_create_nolog": True,
+        }
         group_salemanager = cls.env.ref("sales_team.group_sale_manager")
         group_salesman = cls.env.ref("sales_team.group_sale_salesman")
         group_employee = cls.env.ref("base.group_user")
-        cls.user_manager = cls.env['res.users'].create({
-            'name': 'User Officer',
-            'login': 'user_manager',
-            'email': 'usermanager@test.com',
-            'groups_id': [(6, 0, [group_salemanager.id, group_employee.id])],
-        })
-        cls.user_employee = cls.env['res.users'].create({
-            'name': 'User Empployee',
-            'login': 'user_emp',
-            'email': 'useremp@test.com',
-            'groups_id': [(6, 0, [group_salesman.id, group_employee.id])],
-        })
-        Partner = cls.env['res.partner'].with_context(context_no_mail)
-        cls.partner_customer_usd = Partner.create({
-            'name': 'Customer from the North',
-            'email': 'customer.usd@north.com',
-            'customer_rank': 1,
-        })
+        cls.user_manager = cls.env["res.users"].create(
+            {
+                "name": "User Officer",
+                "login": "user_manager",
+                "email": "usermanager@test.com",
+                "groups_id": [(6, 0, [group_salemanager.id, group_employee.id])],
+            }
+        )
+        cls.user_employee = cls.env["res.users"].create(
+            {
+                "name": "User Empployee",
+                "login": "user_emp",
+                "email": "useremp@test.com",
+                "groups_id": [(6, 0, [group_salesman.id, group_employee.id])],
+            }
+        )
+        Partner = cls.env["res.partner"].with_context(context_no_mail)
+        cls.partner_customer_usd = Partner.create(
+            {
+                "name": "Customer from the North",
+                "email": "customer.usd@north.com",
+                "customer_rank": 1,
+            }
+        )
         # Products
-        uom_unit = cls.env.ref('uom.product_uom_unit')
-        uom_hour = cls.env.ref('uom.product_uom_hour')
-        cls.product_order = cls.env['product.product'].create({
-            'name': "Zed+ Antivirus",
-            'standard_price': 235.0,
-            'list_price': 280.0,
-            'type': 'consu',
-            'uom_id': uom_unit.id,
-            'uom_po_id': uom_unit.id,
-            'invoice_policy': 'order',
-            'expense_policy': 'no',
-            'default_code': 'PROD_ORDER',
-            'service_type': 'manual',
-            'taxes_id': False,
-        })
-        cls.service_deliver = cls.env['product.product'].create({
-            'name': "Cost-plus Contract",
-            'standard_price': 200.0,
-            'list_price': 180.0,
-            'type': 'service',
-            'uom_id': uom_unit.id,
-            'uom_po_id': uom_unit.id,
-            'invoice_policy': 'delivery',
-            'expense_policy': 'no',
-            'default_code': 'SERV_DEL',
-            'service_type': 'manual',
-            'taxes_id': False,
-        })
-        cls.service_order = cls.env['product.product'].create({
-            'name': "Prepaid Consulting",
-            'standard_price': 40.0,
-            'list_price': 90.0,
-            'type': 'service',
-            'uom_id': uom_hour.id,
-            'uom_po_id': uom_hour.id,
-            'invoice_policy': 'order',
-            'expense_policy': 'no',
-            'default_code': 'PRE-PAID',
-            'service_type': 'manual',
-            'taxes_id': False,
+        uom_unit = cls.env.ref("uom.product_uom_unit")
+        uom_hour = cls.env.ref("uom.product_uom_hour")
+        cls.product_order = cls.env["product.product"].create(
+            {
+                "name": "Zed+ Antivirus",
+                "standard_price": 235.0,
+                "list_price": 280.0,
+                "type": "consu",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "order",
+                "expense_policy": "no",
+                "default_code": "PROD_ORDER",
+                "service_type": "manual",
+                "taxes_id": False,
+            }
+        )
+        cls.service_deliver = cls.env["product.product"].create(
+            {
+                "name": "Cost-plus Contract",
+                "standard_price": 200.0,
+                "list_price": 180.0,
+                "type": "service",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "delivery",
+                "expense_policy": "no",
+                "default_code": "SERV_DEL",
+                "service_type": "manual",
+                "taxes_id": False,
+            }
+        )
+        cls.service_order = cls.env["product.product"].create(
+            {
+                "name": "Prepaid Consulting",
+                "standard_price": 40.0,
+                "list_price": 90.0,
+                "type": "service",
+                "uom_id": uom_hour.id,
+                "uom_po_id": uom_hour.id,
+                "invoice_policy": "order",
+                "expense_policy": "no",
+                "default_code": "PRE-PAID",
+                "service_type": "manual",
+                "taxes_id": False,
+            }
+        )
+        cls.product_deliver = cls.env["product.product"].create(
+            {
+                "name": "Switch, 24 ports",
+                "standard_price": 55.0,
+                "list_price": 70.0,
+                "type": "consu",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "delivery",
+                "expense_policy": "no",
+                "default_code": "PROD_DEL",
+                "service_type": "manual",
+                "taxes_id": False,
+            }
+        )
 
-        })
-        cls.product_deliver = cls.env['product.product'].create({
-            'name': "Switch, 24 ports",
-            'standard_price': 55.0,
-            'list_price': 70.0,
-            'type': 'consu',
-            'uom_id': uom_unit.id,
-            'uom_po_id': uom_unit.id,
-            'invoice_policy': 'delivery',
-            'expense_policy': 'no',
-            'default_code': 'PROD_DEL',
-            'service_type': 'manual',
-            'taxes_id': False,
-
-        })
-
-        cls.product_map = OrderedDict([
-            ('prod_order', cls.product_order),
-            ('serv_del', cls.service_deliver),
-            ('serv_order', cls.service_order),
-            ('prod_del', cls.product_deliver),
-        ])
+        cls.product_map = OrderedDict(
+            [
+                ("prod_order", cls.product_order),
+                ("serv_del", cls.service_deliver),
+                ("serv_order", cls.service_order),
+                ("prod_del", cls.product_deliver),
+            ]
+        )
 
     def test_one_step(self):
         self.user_employee.company_id.sudo().so_double_validation = "one_step"
@@ -143,19 +161,44 @@ class TestSaleDoubleValidation(SavepointCase):
         self.assertEquals(so.state, "draft")
 
     def test_two_steps_limit(self):
-        self.user_employee.company_id.sudo().so_double_validation = "two_step"
-        self.user_employee.company_id.sudo().so_double_validation_amount = sum(
+        so_double_validation_amount = sum(
             [2 * p.list_price for (k, p) in self.product_map.items()]
         )
+        self.user_employee.company_id.sudo().so_double_validation = "two_step"
+        self.user_employee.company_id.sudo().so_double_validation_amount = (
+            so_double_validation_amount
+        )
+
+        so_total_price = sum([2 * p.list_price for (_, p) in self.product_map.items()])
+        self.assertEquals(so_double_validation_amount, so_total_price)
+
+        # create quotation
         so = self.create_so_by_user_employee()
-        # confirm quotation
-        state = 'draft'
+
+        # test float compare with currency rounding
+        compare_amount = (
+            float_compare(
+                so_double_validation_amount,
+                so_total_price,
+                precision_rounding=so.currency_id.rounding,
+            )
+            <= 0
+        )
+        self.assertEquals(compare_amount, True)
+
+        # batch of condition tests, to set the SO state to `to_approve`
+        self.assertEquals(so.company_id.so_double_validation, "two_step")
+        self.assertFalse(self.user_employee.has_group("sales_team.group_sale_manager"))
+        self.assertTrue(so.is_amount_to_approve())
+
+        # state must be set to `to_approve` if condition above is satisfied
+        state = "to_approve"
         self.assertEquals(so.state, state)
 
     def test_two_steps_above_limit(self):
         self.user_employee.company_id.sudo().so_double_validation = "two_step"
         self.user_employee.company_id.sudo().so_double_validation_amount = 10
-        # confirm quotation
+        # create quotation
         so = self.create_so_by_user_employee()
         # confirm quotation
         self.assertEquals(so.state, "to_approve")
@@ -165,7 +208,7 @@ class TestSaleDoubleValidation(SavepointCase):
     def test_confirm_so_above_limit_with_user_employee(self):
         self.user_employee.company_id.sudo().so_double_validation = "two_step"
         self.user_employee.company_id.sudo().so_double_validation_amount = 10
-        # confirm quotation
+        # create quotation
         so = self.create_so_by_user_employee()
         # confirm quotation
         self.assertEquals(so.state, "to_approve")
