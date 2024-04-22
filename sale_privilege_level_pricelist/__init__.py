276d4750328a733992bf1359540dbc91aa09ba9a
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-04-22 15:45:00 +0300

    TA#59101 [IMP] sale_privilege_level_pricelist : pricelist post install active

--- sale_privilege_level_pricelist/__init__.py
+++ sale_privilege_level_pricelist/__init__.py
@@ -2,3 +2,15 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def _post_init_hook(cr, registry):
+    """
+    Activate the pricelist feature in the settings.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    config = env["res.config.settings"].create({})
+    config.group_product_pricelist = True
+    config._onchange_group_sale_pricelist()
+    config.execute()
