1e12fe832392158b5338af8515b302dab0a748ea
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-05-15 10:41:17 +0300

    TA#60956 [IMP] Cron improvements

--- sale_warranty/tests/test_expired_warranties_cron.py
+++ sale_warranty/tests/test_expired_warranties_cron.py
@@ -10,35 +10,52 @@ class ExpiredWarrantiesCronCase(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.customer = cls.env['res.partner'].create({
-            'name': 'My Customer',
-        })
 
-        cls.warranty_6_months = cls.env['sale.warranty.type'].create({
-            'name': '6 Months Parts',
-            'duration_in_months': 6,
-            'description': 'Warranted 6 months on parts'
-        })
+        # Set queue job to no delay for testing
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                test_queue_job_no_delay=True,
+            )
+        )
 
-        cls.product_a = cls.env['product.product'].create({
-            'name': 'My Product',
-            'tracking': 'serial',
-            'type': 'product',
-            'warranty_type_ids': [(4, cls.warranty_6_months.id)]
-        })
+        cls.customer = cls.env["res.partner"].create(
+            {
+                "name": "My Customer",
+            }
+        )
+
+        cls.warranty_6_months = cls.env["sale.warranty.type"].create(
+            {
+                "name": "6 Months Parts",
+                "duration_in_months": 6,
+                "description": "Warranted 6 months on parts",
+            }
+        )
+
+        cls.product_a = cls.env["product.product"].create(
+            {
+                "name": "My Product",
+                "tracking": "serial",
+                "type": "product",
+                "warranty_type_ids": [(4, cls.warranty_6_months.id)],
+            }
+        )
 
         cls.today = datetime.now().date()
 
-        cls.warranty = cls.env['sale.warranty'].create({
-            'partner_id': cls.customer.id,
-            'product_id': cls.product_a.id,
-            'type_id': cls.warranty_6_months.id,
-            'state': 'active',
-            'activation_date': cls.today - timedelta(90),
-        })
+        cls.warranty = cls.env["sale.warranty"].create(
+            {
+                "partner_id": cls.customer.id,
+                "product_id": cls.product_a.id,
+                "type_id": cls.warranty_6_months.id,
+                "state": "active",
+                "activation_date": cls.today - timedelta(90),
+            }
+        )
 
     def run_cron(self):
-        self.env.ref('sale_warranty.expired_warranties_cron').method_direct_trigger()
+        self.env.ref("sale_warranty.expired_warranties_cron").method_direct_trigger()
 
 
 class TestExpiredWarrantiesCron(ExpiredWarrantiesCronCase):
@@ -46,14 +63,14 @@ class TestExpiredWarrantiesCron(ExpiredWarrantiesCronCase):
     def test_if_expiration_date_after_today_then_warranty_active(self):
         self.warranty.expiry_date = self.today + timedelta(1)
         self.run_cron()
-        assert self.warranty.state == 'active'
+        assert self.warranty.state == "active"
 
     def test_if_expiration_date_is_today_then_warranty_active(self):
         self.warranty.expiry_date = self.today
         self.run_cron()
-        assert self.warranty.state == 'active'
+        assert self.warranty.state == "active"
 
     def test_if_expiration_date_before_today_then_warranty_expired(self):
         self.warranty.expiry_date = self.today - timedelta(1)
         self.run_cron()
-        assert self.warranty.state == 'expired'
+        assert self.warranty.state == "expired"
