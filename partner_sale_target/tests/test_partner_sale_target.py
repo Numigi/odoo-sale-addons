ea90448b44a1da27d2dd922ebf045621161157c1
Rivo Lalaina <rivo2302@gmail.com>
2024-09-02 13:14:43 +0300

    TA#65204 [IMP] partner_sale_target : unit testing

--- partner_sale_target/tests/test_partner_sale_target.py
+++ partner_sale_target/tests/test_partner_sale_target.py
@@ -11,39 +11,32 @@ class TestPartnerSaleTarget(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
 
-    def test_partner_sale_target(self):
-        partner = self.env["res.partner"].create(
+        cls.partner = cls.env["res.partner"].create(
             {
                 "name": "Test Partner",
-                "company_type": "person",
+                "company_type": "company"
             }
         )
-        partner.write({"company_type": "company"})
-        self.assertTrue(partner.is_sale_target_allowed_contact)
 
-        target_1 = self.env["sale.target"].create(
+        cls.target_1 = cls.env["sale.target"].create(
             {
-                "partner_id": partner.id,
+                "partner_id": cls.partner.id,
                 "date_start": "2023-01-01",
                 "date_end": fields.Date.today(),
                 "sale_target": 500,
             }
         )
 
-        # Check that the realized target is updated
-        self.assertTrue(target_1.partner_id.is_sale_target_allowed_contact)
-
-        # Create sale order that is linked to the sale target
-        sale_order_1 = self.env["sale.order"].create(
+        cls.sale_order_1 = cls.env["sale.order"].create(
             {
-                "partner_id": partner.id,
+                "partner_id": cls.partner.id,
                 "date_order": "2023-06-01",
                 "order_line": [
                     (
                         0,
                         0,
                         {
-                            "product_id": self.env.ref("product.product_product_4").id,
+                            "product_id": cls.env.ref("product.product_product_4").id,
                             "product_uom_qty": 1,
                             "price_unit": 100,
                         },
@@ -51,24 +44,30 @@ class TestPartnerSaleTarget(SavepointCase):
                 ],
             }
         )
-        # Confirm the sale order
-        sale_order_1.action_confirm()
-        sale_order_1.write({"date_order": "2023-06-20"})
+        cls.sale_order_1.action_confirm()
+        cls.sale_order_1.write({"date_order": "2023-06-20"})
 
-        # Check partner have sale_order_1_ids and its state is sale
-        self.assertTrue(target_1.partner_id.sale_order_ids)
-        self.assertEqual(target_1.partner_id.sale_order_ids[0].state, "sale")
+    def test_is_sale_target_allowed_contact(self):
+        self.assertTrue(self.partner.is_sale_target_allowed_contact)
 
-        self.assertEqual(target_1.realized_target, 100)
+    def test_realized_target_calculation(self):
+        self.assertEqual(self.target_1.realized_target, 100)
 
-        # Check the current sale target and current realized target
-        self.assertEqual(partner.current_sale_target, 500)
-        self.assertEqual(partner.current_realized_target, 0.2)  # 100/500
+    def test_current_sale_target_and_realized(self):
+        self.target_1.write({
+            "date_start": "2023-01-02",
+        })
 
-        # Create a new sale target
+        # Keep in mind that the current sale target is the sum of all sale targets
+        # that are active today or have an end date greater than today
+        # so even having old sale targets, the current sale must be the same
+        self.assertEqual(self.partner.current_sale_target, 500)
+        self.assertEqual(self.partner.current_realized_target, 0.2)  # 100/500
+
+    def test_create_new_target(self):
         target_2 = self.env["sale.target"].create(
             {
-                "partner_id": partner.id,
+                "partner_id": self.partner.id,
                 "date_start": "2022-01-01",
                 "date_end": "2022-05-20",
                 "sale_target": 300,
@@ -77,7 +76,7 @@ class TestPartnerSaleTarget(SavepointCase):
 
         sale_order_2 = self.env["sale.order"].create(
             {
-                "partner_id": partner.id,
+                "partner_id": self.partner.id,
                 "date_order": "2022-02-01",
                 "order_line": [
                     (
@@ -92,24 +91,18 @@ class TestPartnerSaleTarget(SavepointCase):
                 ],
             }
         )
-        # Confirm the sale order
         sale_order_2.action_confirm()
         sale_order_2.write({"date_order": "2022-02-20"})
 
         self.assertEqual(target_2.realized_target, 200)
+        self.assertEqual(self.partner.current_sale_target, 500)
+        self.assertEqual(self.partner.current_realized_target, 0.2)
 
-        # Keep in mind that the current sale target is the sum of all sale targets
-        # that are active today or have an end date greater than today
-        # so even having old sale targets, the current sale must be the same
-        self.assertEqual(partner.current_sale_target, 500)
-        self.assertEqual(partner.current_realized_target, 0.2)
-
-        # Create sale target line with overlapping dates and check
-        # if it raises a validation error
+    def test_overlapping_target_validation(self):
         with self.assertRaises(ValidationError):
             self.env["sale.target"].create(
                 {
-                    "partner_id": partner.id,
+                    "partner_id": self.partner.id,
                     "date_start": "2023-06-01",
                     "date_end": fields.Date.today(),
                     "sale_target": 500,
