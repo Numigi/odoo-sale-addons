e6a5f32ae8331d3ea13c9edf615f4e2a3c8942cf
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-01-27 17:32:35 +0300

    TA#72150 [16.0][MIG][MOV] sale_product_extra_views : move from Numigi product repository, known as product_extra_views_sale (#394)
    * TA#72150 [16.0][MIG][MOV] sale_product_extra_views : move from Numigi product repository, known as product_extra_views_sale

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -1,17 +1,15 @@
-# © 2024 Numigi
+# Copyright 2024 Numigi
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Main Module",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://www.numigi.com",
     "license": "LGPL-3",
     "category": "Other",
     "summary": "Install all addons required for testing.",
-    "depends": [
-        "sale_management",
-    ],
+    "depends": ["sale_management", "sale_product_extra_views"],
     "installable": True,
 }
---
+++ sale_product_extra_views/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Sale Product Extra Views",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Product",
+    "summary": "Add pivot and graph views to products from sales",
+    "depends": [
+        "product_extra_views",
+        "sale_management",
+    ],
+    "data": [
+        "data/ir_actions_data.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
