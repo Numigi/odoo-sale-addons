97ad07a25ac9f32f8fbbe3d440ba16ebf2afc442
majouda <majdaelmariouli@gmail.com>
2025-02-13 14:02:00 -0500

    Code Quanlity

--- sale_purchase_procurement_link/models/purchase_order_line.py
+++ sale_purchase_procurement_link/models/purchase_order_line.py
@@ -1,10 +1,7 @@
 # Copyright 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
-
-import logging
-_logger = logging.getLogger(__name__)
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
