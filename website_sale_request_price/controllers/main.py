dc44d066602c9e4f8d95f81715ea6923183bfc1d
Abdellatif Benzbiria <odoo-base@numigi.com>
2025-10-27 13:18:56 +0100

    [IMP] website_sale_request_price : secure read is_request_price_required product price

--- website_sale_request_price/controllers/main.py
+++ website_sale_request_price/controllers/main.py
@@ -2,7 +2,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import werkzeug
-
+import json
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo import http
 from odoo.http import request
 
@@ -31,3 +32,58 @@ class WebsiteSaleRequestPriceController(http.Controller):
     def request_price_confirm(self, **post):
         request.env["crm.lead"].create_website_sale_request(post)
         return werkzeug.utils.redirect(request.httprequest.referrer)
+
+
+
+
+
+class WebsiteSaleRequestPriceExtended(WebsiteSale):
+
+    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'],
+        website=True, csrf=False)
+    def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
+        product = request.env['product.product'].browse(int(product_id))
+
+        if product.is_request_price_required:
+            return werkzeug.utils.redirect('/shop')
+
+        return super(WebsiteSaleRequestPriceExtended, self).cart_update(
+            product_id=product_id, add_qty=add_qty, set_qty=set_qty, **kw)
+
+    @http.route(['/shop/cart/update_json'], type='json', auth="public",
+        methods=['POST'], website=True, csrf=False)
+    def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None,
+                         display=True):
+        product = request.env['product.product'].browse(int(product_id))
+        if product.is_request_price_required:
+            return {'error': 'This product needs a  price request '}
+
+        return super(WebsiteSaleRequestPriceExtended, self).cart_update_json(
+            product_id=product_id, line_id=line_id, add_qty=add_qty, set_qty=set_qty,
+            display=display)
+
+    @http.route(['/shop/cart/update_option'], type='http', auth="public",
+        methods=['POST'], website=True)
+    def cart_options_update_json(self, product_and_options, goto_shop=None, lang=None,
+                                 **post):
+        product_and_options = json.loads(product_and_options)
+        for product_option in product_and_options:
+            product_id = product_option.get('product_id')
+            if product_id:
+                product = request.env['product.product'].browse(int(product_id))
+                if product.is_request_price_required:
+                    return werkzeug.utils.redirect('/shop')
+
+        return super(WebsiteSaleRequestPriceExtended, self).cart_options_update_json(
+            product_and_options=product_and_options, goto_shop=goto_shop, lang=lang,
+            **post)
+
+    @http.route('/shop/product/<model("product.template"):product>', type='http',
+        auth="public", website=True)
+    def product(self, product, category='', search='', **kwargs):
+        """Surcharge pour vérifier l'accès au produit"""
+        if product.is_request_price_required:
+            pass
+        return super(WebsiteSaleRequestPriceExtended, self).product(product, category,
+            search, **kwargs)
+
---
+++ website_sale_request_price/models/sale_order.py
@@ -0,0 +1,27 @@
+# © Numigi (tm) and all its contributors (https://numigi.com/r/home)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0,
+                     **kwargs):
+        if product_id:
+            product = self.env['product.product'].browse(product_id)
+            if product.is_request_price_required:
+                raise ValidationError("This product needs a price request")
+        product_options = kwargs.get('product_options', [])
+        for option in product_options:
+            option_product_id = option.get('product_id')
+            if option_product_id:
+                option_product = self.env['product.product'].browse(option_product_id)
+                if option_product.is_request_price_required:
+                    raise ValidationError(
+                        "This product needs a price request")
+        return super(SaleOrder, self)._cart_update(product_id=product_id,
+            line_id=line_id, add_qty=add_qty, set_qty=set_qty, **kwargs)
