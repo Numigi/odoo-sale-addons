a4301543a5def95fb6612bef6ad3e0279463d635
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-04-02 17:36:11 +0000

    Revert "TA#83774 [IMP] website_sale_request_price : cart_options_update_json …" (#419)
    This reverts commit 02957bc041f7ce66f5cbfcde29300b2f10ee54c2.

--- website_sale_request_price/controllers/main.py
+++ website_sale_request_price/controllers/main.py
@@ -10,66 +10,41 @@ from odoo.http import request
 
 class WebsiteSaleRequestPriceExtended(WebsiteSale):
 
-    @http.route(
-        ["/shop/cart/update"],
-        type="http",
-        auth="public",
-        methods=["POST"],
-        website=True,
-        csrf=False,
-    )
+    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'],
+                website=True, csrf=False)
     def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
-        product = request.env["product.product"].browse(int(product_id))
+        product = request.env['product.product'].browse(int(product_id))
 
         if product.is_request_price_required:
-            return werkzeug.utils.redirect("/shop")
+            return werkzeug.utils.redirect('/shop')
 
         return super(WebsiteSaleRequestPriceExtended, self).cart_update(
-            product_id=product_id, add_qty=add_qty, set_qty=set_qty, **kw
-        )
+            product_id=product_id, add_qty=add_qty, set_qty=set_qty, **kw)
 
-    @http.route(
-        ["/shop/cart/update_json"],
-        type="json",
-        auth="public",
-        methods=["POST"],
-        website=True,
-        csrf=False,
-    )
+    @http.route(['/shop/cart/update_json'], type='json', auth="public",
+                methods=['POST'], website=True, csrf=False)
     def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None,
                          display=True):
-        product = request.env["product.product"].browse(int(product_id))
+        product = request.env['product.product'].browse(int(product_id))
         if product.is_request_price_required:
-            return {"error": "This product needs a price request"}
+            return {'error': 'This product needs a price request'}
 
         return super(WebsiteSaleRequestPriceExtended, self).cart_update_json(
-            product_id=product_id,
-            line_id=line_id,
-            add_qty=add_qty,
-            set_qty=set_qty,
-            display=display,
-        )
+            product_id=product_id, line_id=line_id, add_qty=add_qty, set_qty=set_qty,
+            display=display)
 
-    @http.route(
-        ["/shop/cart/update_option"],
-        type="http",
-        auth="public",
-        methods=["POST"],
-        website=True,
-    )
-    def cart_options_update_json(self, product_and_options, goto_shop=None, lang=None, **post):
-        # Parse the JSON string into a new variable to avoid overwriting the original parameter.
-        # The core Odoo method relies on 'product_and_options' remaining a raw JSON string.
-        parsed_options = json.loads(product_and_options)
-
-        for product_option in parsed_options:
-            product_id = product_option.get("product_id")
+    @http.route(['/shop/cart/update_option'], type='http', auth="public",
+                methods=['POST'], website=True)
+    def cart_options_update_json(self, product_and_options, goto_shop=None, lang=None,
+                                 **post):
+        product_and_options = json.loads(product_and_options)
+        for product_option in product_and_options:
+            product_id = product_option.get('product_id')
             if product_id:
-                product = request.env["product.product"].browse(int(product_id))
+                product = request.env['product.product'].browse(int(product_id))
                 if product.is_request_price_required:
-                    return werkzeug.utils.redirect("/shop")
+                    return werkzeug.utils.redirect('/shop')
 
-        # Pass the original, unparsed JSON string to the upstream method
         return super(WebsiteSaleRequestPriceExtended, self).cart_options_update_json(
-            product_and_options=product_and_options, goto_shop=goto_shop, lang=lang, **post
-        )
+            product_and_options=product_and_options, goto_shop=goto_shop, lang=lang,
+            **post)
